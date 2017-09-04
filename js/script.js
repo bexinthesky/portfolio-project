217dcb2bd6b8dc603be11937f8c1301758e22e96
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-09-04 11:37:39 -0700

    changed contact form area, added JS for contact form, made a comment of alternative contact form, put basic html text in ucsd

---
+++ js/script.js
@@ -0,0 +1,44 @@
+(function(){
+  var myForm = document.getElementById('emailForm');
+  var submitButton = document.getElementById('submitButton');
+
+  var nameInput = document.getElementById('name');
+  var emailInput = document.getElementById('email');
+  var messageInput = document.getElementById('message');
+  var subjectInput = document.getElementById('subject');
+
+  messageInput.textContent = '';
+
+  console.log(messageInput.attributes);
+
+  nameInput.addEventListener('blur', function(event){
+    var myName = nameInput.value;
+    var validName = /^[0-9a-zA-Z\s]+$/.test(myName);
+    console.log(myName);
+    if(!validName){
+      nameInput.classList.add('danger');
+      submitButton.disabled = true;
+    } else {
+      nameInput.classList.remove('danger');
+      submitButton.disabled = false;
+    }
+  });
+
+  emailInput.addEventListener('blur', function(event){
+    var myEmail = emailInput.value;
+    var validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(myEmail);
+    console.log(myEmail);
+    if(!validEmail){
+      emailInput.classList.add('danger');
+      submitButton.disabled = true;
+    } else {
+      emailInput.classList.remove('danger');
+      submitButton.disabled = false;
+    }
+  });
+
+  myForm.addEventListener('submit', function(event) {
+    document.getElementById('message').value.trim();
+    myForm.setAttribute('action', 'https://formspree.io/rahorwitz3@gmail.com');
+  });
+}());
